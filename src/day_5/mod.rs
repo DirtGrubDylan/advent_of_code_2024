2b522a8266122a0d842f8a5412b6ad39ba3681f3
Dylan Hicks <dman1366@gmail.com>
2024-12-06 01:25:47 -0700

    Finish day 5

--- src/day_5/mod.rs
+++ src/day_5/mod.rs
@@ -1,18 +1,121 @@
+mod safety_manual;
+
 use crate::util::file_reader::to_string_vector;
+use std::collections::HashSet;
+
+use safety_manual::{PageOrderingRuleLine, PageOrderingRules};
 
 pub fn run() {
     let input = to_string_vector("inputs/day_5.txt").expect("Something went wrong with Day 5!");
 
-    println!("Day 5 Part 1: {:?}", part_1(&input));
-    println!("Day 5 Part 2: {:?}", part_2(&input));
+    let (rules, pages_to_print) = parse_data(&input);
+
+    println!("Day 5 Part 1: {:?}", part_1(&rules, &pages_to_print));
+    println!("Day 5 Part 2: {:?}", part_2(&rules, &pages_to_print));
 }
 
-fn part_1(puzzle: &[String]) -> usize {
-    unimplemented!()
+fn part_1(rules: &PageOrderingRules, pages_to_print: &[Vec<u32>]) -> u32 {
+    pages_to_print
+        .iter()
+        .filter(|line| pages_to_print_line_is_valid(rules, line))
+        .map(|line| line.get(line.len() / 2).unwrap())
+        .sum()
 }
 
-fn part_2(puzzle: &[String]) -> usize {
-    unimplemented!()
+fn part_2(rules: &PageOrderingRules, pages_to_print: &[Vec<u32>]) -> u32 {
+    pages_to_print
+        .iter()
+        .filter(|line| !pages_to_print_line_is_valid(rules, line))
+        .map(|line| fix_line(rules, line))
+        .map(|line| *line.get(line.len() / 2).unwrap())
+        .sum()
+}
+
+fn pages_to_print_line_is_valid(rules: &PageOrderingRules, line: &[u32]) -> bool {
+    let mut valid_so_far = true;
+    let mut pages_so_far = HashSet::new();
+
+    for &page_number in line {
+        if !valid_so_far {
+            break;
+        }
+
+        valid_so_far = rules.pages_before_is_valid(page_number, &pages_so_far);
+
+        pages_so_far.insert(page_number);
+    }
+
+    valid_so_far
+}
+
+fn fix_line(rules: &PageOrderingRules, line: &[u32]) -> Vec<u32> {
+    let mut result = line.to_vec();
+
+    loop {
+        let mut current_index = 0;
+        let mut conflicts = HashSet::new();
+        let mut pages_so_far = HashSet::new();
+
+        for &page_number in &result {
+            conflicts = rules.conflicting_pages(page_number, &pages_so_far);
+
+            if !conflicts.is_empty() {
+                break;
+            }
+
+            current_index += 1;
+            pages_so_far.insert(page_number);
+        }
+
+        if conflicts.is_empty() {
+            break;
+        }
+
+        let conflict_with_lowest_index = result
+            .iter()
+            .position(|number| conflicts.contains(number))
+            .unwrap();
+
+        let problem_page_number = result.remove(current_index);
+
+        result.insert(conflict_with_lowest_index, problem_page_number);
+    }
+
+    result
+}
+
+fn parse_data(input: &[String]) -> (PageOrderingRules, Vec<Vec<u32>>) {
+    let mut split_data = input.split(String::is_empty);
+
+    let rules_data = split_data.next().expect("No rules data!");
+    let pages_data = split_data.next().expect("No rules data!");
+
+    (parse_rules(rules_data), parse_pages_to_print(pages_data))
+}
+
+fn parse_rules(rules: &[String]) -> PageOrderingRules {
+    let mut result = PageOrderingRules::new();
+
+    for rule_line in parse_rule_lines(rules) {
+        result.add_rule_from_line(rule_line);
+    }
+
+    result
+}
+
+fn parse_rule_lines(rules: &[String]) -> Vec<PageOrderingRuleLine> {
+    rules.iter().map(|line| line.parse().unwrap()).collect()
+}
+
+fn parse_pages_to_print(lines: &[String]) -> Vec<Vec<u32>> {
+    lines
+        .iter()
+        .map(|line| {
+            line.split(',')
+                .map(|number| number.parse().unwrap())
+                .collect()
+        })
+        .collect()
 }
 
 #[cfg(test)]
@@ -23,13 +126,203 @@ mod tests {
     fn test_part_1() {
         let input = to_string_vector("test_inputs/day_5.txt").unwrap();
 
-        assert_eq!(part_1(&input), 18);
+        let (rules, pages_to_print) = parse_data(&input);
+
+        assert_eq!(part_1(&rules, &pages_to_print), 143);
     }
 
     #[test]
     fn test_part_2() {
         let input = to_string_vector("test_inputs/day_5.txt").unwrap();
 
-        assert_eq!(part_2(&input), 9);
+        let (rules, pages_to_print) = parse_data(&input);
+
+        assert_eq!(part_2(&rules, &pages_to_print), 123);
+    }
+
+    #[test]
+    fn test_pages_to_print_line_is_valid_true() {
+        let input_rule_lines = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+            String::from("75|29"),
+            String::from("61|13"),
+            String::from("75|53"),
+            String::from("29|13"),
+            String::from("97|29"),
+            String::from("53|29"),
+            String::from("61|53"),
+            String::from("97|53"),
+            String::from("61|29"),
+            String::from("47|13"),
+            String::from("75|47"),
+            String::from("97|75"),
+            String::from("47|61"),
+            String::from("75|61"),
+            String::from("47|29"),
+            String::from("75|13"),
+            String::from("53|13"),
+        ];
+
+        let input_rules = parse_rules(&input_rule_lines);
+        let input_pages_to_print_line = [75, 47, 61, 53, 29];
+
+        let result = pages_to_print_line_is_valid(&input_rules, &input_pages_to_print_line);
+
+        assert!(result);
+    }
+
+    #[test]
+    fn test_pages_to_print_line_is_valid_false() {
+        let input_rule_lines = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+            String::from("75|29"),
+            String::from("61|13"),
+            String::from("75|53"),
+            String::from("29|13"),
+            String::from("97|29"),
+            String::from("53|29"),
+            String::from("61|53"),
+            String::from("97|53"),
+            String::from("61|29"),
+            String::from("47|13"),
+            String::from("75|47"),
+            String::from("97|75"),
+            String::from("47|61"),
+            String::from("75|61"),
+            String::from("47|29"),
+            String::from("75|13"),
+            String::from("53|13"),
+        ];
+
+        let input_rules = parse_rules(&input_rule_lines);
+        let input_pages_to_print_line = [75, 97, 47, 61, 53];
+
+        let result = pages_to_print_line_is_valid(&input_rules, &input_pages_to_print_line);
+
+        assert!(!result);
+    }
+
+    #[test]
+    fn test_fix_line() {
+        let input_rule_lines = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+            String::from("75|29"),
+            String::from("61|13"),
+            String::from("75|53"),
+            String::from("29|13"),
+            String::from("97|29"),
+            String::from("53|29"),
+            String::from("61|53"),
+            String::from("97|53"),
+            String::from("61|29"),
+            String::from("47|13"),
+            String::from("75|47"),
+            String::from("97|75"),
+            String::from("47|61"),
+            String::from("75|61"),
+            String::from("47|29"),
+            String::from("75|13"),
+            String::from("53|13"),
+        ];
+
+        let input_rules = parse_rules(&input_rule_lines);
+        let input_pages_to_print_line = [75, 61, 13, 97, 29];
+
+        let expected = vec![97, 75, 61, 29, 13];
+
+        let result = fix_line(&input_rules, &input_pages_to_print_line);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_rule_lines() {
+        let input = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+        ];
+
+        let expected = vec![
+            PageOrderingRuleLine::new(47, 53),
+            PageOrderingRuleLine::new(97, 13),
+            PageOrderingRuleLine::new(97, 61),
+            PageOrderingRuleLine::new(97, 47),
+        ];
+
+        let result = parse_rule_lines(&input);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_rules() {
+        let input = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+        ];
+
+        let mut expected = PageOrderingRules::new();
+
+        expected.add_rule_from_line(PageOrderingRuleLine::new(47, 53));
+        expected.add_rule_from_line(PageOrderingRuleLine::new(97, 13));
+        expected.add_rule_from_line(PageOrderingRuleLine::new(97, 61));
+        expected.add_rule_from_line(PageOrderingRuleLine::new(97, 47));
+
+        let result = parse_rules(&input);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_pages_to_print() {
+        let input = [
+            String::from("75,47,61,53,29"),
+            String::from("97,61,53,29,13"),
+        ];
+
+        let expected = vec![vec![75, 47, 61, 53, 29], vec![97, 61, 53, 29, 13]];
+
+        let result = parse_pages_to_print(&input);
+
+        assert_eq!(result, expected);
+    }
+
+    #[test]
+    fn test_parse_data() {
+        let input = [
+            String::from("47|53"),
+            String::from("97|13"),
+            String::from("97|61"),
+            String::from("97|47"),
+            String::new(),
+            String::from("75,47,61,53,29"),
+            String::from("97,61,53,29,13"),
+        ];
+
+        let mut expected_rules = PageOrderingRules::new();
+
+        expected_rules.add_rule_from_line(PageOrderingRuleLine::new(47, 53));
+        expected_rules.add_rule_from_line(PageOrderingRuleLine::new(97, 13));
+        expected_rules.add_rule_from_line(PageOrderingRuleLine::new(97, 61));
+        expected_rules.add_rule_from_line(PageOrderingRuleLine::new(97, 47));
+
+        let expected_pages = vec![vec![75, 47, 61, 53, 29], vec![97, 61, 53, 29, 13]];
+
+        let result = parse_data(&input);
+
+        assert_eq!(result, (expected_rules, expected_pages));
     }
 }
---
+++ src/day_5/safety_manual.rs
@@ -0,0 +1,141 @@
+use std::collections::{HashMap, HashSet};
+use std::str::FromStr;
+
+#[derive(Debug, PartialEq)]
+pub struct PageOrderingRules {
+    rules: HashMap<u32, PageOrderingRule>,
+}
+
+impl PageOrderingRules {
+    pub fn new() -> Self {
+        PageOrderingRules {
+            rules: HashMap::new(),
+        }
+    }
+
+    pub fn add_rule_from_line(&mut self, line: PageOrderingRuleLine) -> bool {
+        self.rules
+            .entry(line.before_page_number)
+            .or_insert(PageOrderingRule::new(line.before_page_number))
+            .add_page_after(line.after_page_number)
+    }
+
+    pub fn pages_before_is_valid(&self, page_number: u32, pages_before: &HashSet<u32>) -> bool {
+        self.rules
+            .get(&page_number)
+            .map_or(true, |rule| !rule.contains_after_pages_in(pages_before))
+    }
+
+    pub fn conflicting_pages(&self, page_number: u32, pages_before: &HashSet<u32>) -> HashSet<u32> {
+        self.rules.get(&page_number).map_or(HashSet::new(), |rule| {
+            rule.after_pages_contained_in(pages_before)
+        })
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct PageOrderingRule {
+    page_number: u32,
+    pages_after: HashSet<u32>,
+}
+
+impl PageOrderingRule {
+    pub fn new(page_number: u32) -> Self {
+        PageOrderingRule {
+            page_number,
+            pages_after: HashSet::new(),
+        }
+    }
+
+    pub fn add_page_after(&mut self, page_number: u32) -> bool {
+        self.pages_after.insert(page_number)
+    }
+
+    pub fn contains_after_pages_in(&self, page_numbers: &HashSet<u32>) -> bool {
+        !self.pages_after.is_disjoint(page_numbers)
+    }
+
+    pub fn after_pages_contained_in(&self, page_numbers: &HashSet<u32>) -> HashSet<u32> {
+        self.pages_after
+            .intersection(page_numbers)
+            .copied()
+            .collect()
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParsePageOrderingRuleError;
+
+impl FromStr for PageOrderingRule {
+    type Err = ParsePageOrderingRuleError;
+
+    fn from_str(page_number_str: &str) -> Result<Self, Self::Err> {
+        page_number_str
+            .parse()
+            .map(PageOrderingRule::new)
+            .map_err(|_| ParsePageOrderingRuleError)
+    }
+}
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub struct PageOrderingRuleLine {
+    before_page_number: u32,
+    after_page_number: u32,
+}
+
+impl PageOrderingRuleLine {
+    pub fn new(before_page_number: u32, after_page_number: u32) -> Self {
+        PageOrderingRuleLine {
+            before_page_number,
+            after_page_number,
+        }
+    }
+
+    fn from_str_parts(before: &str, after: &str) -> Result<Self, ParsePageOrderingRuleLineError> {
+        match (before.parse(), after.parse()) {
+            (Ok(before_num), Ok(after_num)) => Ok(PageOrderingRuleLine::new(before_num, after_num)),
+            _ => Err(ParsePageOrderingRuleLineError),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct ParsePageOrderingRuleLineError;
+
+impl FromStr for PageOrderingRuleLine {
+    type Err = ParsePageOrderingRuleLineError;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        line.split_once('|')
+            .ok_or(ParsePageOrderingRuleLineError)
+            .and_then(|(before, after)| PageOrderingRuleLine::from_str_parts(before, after))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_page_ordering_rule_line_from_str() {
+        let expected_ok = Ok(PageOrderingRuleLine::new(32, 12));
+        let expected_err = Err(ParsePageOrderingRuleLineError);
+
+        let result_ok = "32|12".parse();
+        let result_err = "t2|12".parse::<PageOrderingRuleLine>();
+
+        assert_eq!(result_ok, expected_ok);
+        assert_eq!(result_err, expected_err);
+    }
+    #[test]
+    fn test_page_ordering_rule_from_str() {
+        let expected_ok = Ok(PageOrderingRule::new(32));
+        let expected_err = Err(ParsePageOrderingRuleError);
+
+        let result_ok = "32".parse();
+        let result_err = "t2".parse::<PageOrderingRule>();
+
+        assert_eq!(result_ok, expected_ok);
+        assert_eq!(result_err, expected_err);
+    }
+}
