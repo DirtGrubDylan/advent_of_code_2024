ab4ee5e64a142746b50a9c664c83bddadc341f6d
Dylan Hicks <dylanhicks@google.com>
2024-12-09 22:16:43 +0000

    Finshed

--- src/day_7/mod.rs
+++ src/day_7/mod.rs
@@ -1,18 +1,36 @@
+mod calibration;
+
 use crate::util::file_reader::to_string_vector;
+
+use calibration::Equation;
 
 pub fn run() {
     let input = to_string_vector("inputs/day_7.txt").expect("Something went wrong with Day 7!");
 
-    println!("Day 7 Part 1: {:?}", part_1(&input));
-    println!("Day 7 Part 2: {:?}", part_2(&input));
+    let equations = get_equations(&input);
+
+    println!("Day 7 Part 1: {:?}", part_1(&equations));
+    println!("Day 7 Part 2: {:?}", part_2(&equations));
 }
 
-fn part_1(_input: &[String]) -> usize {
-    unimplemented!()
+fn part_1(equations: &[Equation]) -> usize {
+    equations
+        .iter()
+        .filter(|equation| equation.has_valid_solutions(/*allow_concat=*/ false))
+        .map(|equation| equation.test_value)
+        .sum()
 }
 
-fn part_2(_input: &[String]) -> usize {
-    unimplemented!()
+fn part_2(equations: &[Equation]) -> usize {
+    equations
+        .iter()
+        .filter(|equation| equation.has_valid_solutions(/*allow_concat=*/ true))
+        .map(|equation| equation.test_value)
+        .sum()
+}
+
+fn get_equations(input: &[String]) -> Vec<Equation> {
+    input.iter().filter_map(|line| line.parse().ok()).collect()
 }
 
 #[cfg(test)]
@@ -20,18 +38,20 @@ mod tests {
     use super::*;
 
     #[test]
-    #[should_panic(expected = "File not found!")]
     fn test_part_1() {
         let input = to_string_vector("test_inputs/day_7.txt").unwrap();
 
-        assert_eq!(part_2(&input), 666);
+        let equations = get_equations(&input);
+
+        assert_eq!(part_1(&equations), 3_749);
     }
 
     #[test]
-    #[should_panic(expected = "File not found!")]
     fn test_part_2() {
         let input = to_string_vector("test_inputs/day_7.txt").unwrap();
 
-        assert_eq!(part_2(&input), 666);
+        let equations = get_equations(&input);
+
+        assert_eq!(part_2(&equations), 11_387);
     }
 }
