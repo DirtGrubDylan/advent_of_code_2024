a1788598b9a393201e96bb1ec58659e8e59eb995
Dylan Hicks <dman1366@gmail.com>
2024-12-13 13:42:05 -0700

    Finish day 10!

--- src/day_10/hiking.rs
+++ src/day_10/hiking.rs
@@ -6,34 +6,6 @@ const RIGHT: Point2d<i32> = Point2d { x: 1, y: 0 };
 const DOWN: Point2d<i32> = Point2d { x: 0, y: 1 };
 const LEFT: Point2d<i32> = Point2d { x: -1, y: 0 };
 
-#[derive(Debug, PartialEq, Eq, Copy, Clone, Hash)]
-pub enum Direction {
-    Up,
-    Right,
-    Down,
-    Left,
-}
-
-impl Direction {
-    fn as_offset(self) -> Point2d<i32> {
-        match self {
-            Direction::Up => UP,
-            Direction::Right => RIGHT,
-            Direction::Down => DOWN,
-            Direction::Left => LEFT,
-        }
-    }
-
-    fn turn_90_degrees_clockwise(self) -> Self {
-        match self {
-            Direction::Up => Direction::Right,
-            Direction::Right => Direction::Down,
-            Direction::Down => Direction::Left,
-            Direction::Left => Direction::Up,
-        }
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub struct Map {
     heights: HashMap<Point2d<i32>, u32>,
@@ -56,42 +28,73 @@ impl Map {
         Map { heights }
     }
 
-    fn number_of_peak_paths_from(&self, point: Point2d<i32>) -> u32 {
-        // match self.heights.get(&point) {
-        //     None | Some(height) if height
-        // }
-
-        unimplemented!()
+    pub fn number_of_reachable_peaks(&self) -> usize {
+        self.bases()
+            .into_iter()
+            .map(|base| self.number_of_reachable_peaks_from(base))
+            .sum()
     }
 
-    fn number_of_valid_paths_from(
+    pub fn number_of_distinct_valid_paths(&self) -> usize {
+        self.bases()
+            .into_iter()
+            .map(|base| self.number_of_distinct_valid_paths_from(base))
+            .sum()
+    }
+
+    fn number_of_reachable_peaks_from(&self, point: Point2d<i32>) -> usize {
+        let mut valid_reachable_peaks = HashSet::new();
+
+        valid_reachable_peaks.extend(self.valid_reachable_peaks_from(point, point + UP));
+        valid_reachable_peaks.extend(self.valid_reachable_peaks_from(point, point + RIGHT));
+        valid_reachable_peaks.extend(self.valid_reachable_peaks_from(point, point + DOWN));
+        valid_reachable_peaks.extend(self.valid_reachable_peaks_from(point, point + LEFT));
+
+        valid_reachable_peaks.len()
+    }
+
+    fn number_of_distinct_valid_paths_from(&self, point: Point2d<i32>) -> usize {
+        let mut peaks_with_distinct_path = Vec::new();
+
+        peaks_with_distinct_path.extend(self.valid_reachable_peaks_from(point, point + UP));
+        peaks_with_distinct_path.extend(self.valid_reachable_peaks_from(point, point + RIGHT));
+        peaks_with_distinct_path.extend(self.valid_reachable_peaks_from(point, point + DOWN));
+        peaks_with_distinct_path.extend(self.valid_reachable_peaks_from(point, point + LEFT));
+
+        peaks_with_distinct_path.len()
+    }
+
+    fn valid_reachable_peaks_from(
         &self,
         current_point: Point2d<i32>,
         next_point: Point2d<i32>,
-    ) -> u32 {
+    ) -> Vec<Point2d<i32>> {
+        let mut result = Vec::new();
+
         let current_height = self
             .heights
             .get(&current_point)
-            .expect(&format!("Map does not have {current_point:?}"));
+            .unwrap_or_else(|| panic!("Map does not have {current_point:?}"));
 
         match self.heights.get(&next_point) {
-            Some(9) => 1,
-            Some(next_height) if next_height == current_height + 1 => {
-                self.number_of_valid_paths_from(next_point, next_point + UP)
-                    + self.number_of_valid_paths_from(next_point, next_point + RIGHT)
-                    + self.number_of_valid_paths_from(next_point, next_point + DOWN)
-                    + self.number_of_valid_paths_from(next_point, next_point + LEFT)
+            None => {}
+            Some(&next_height) if next_height != current_height + 1 => {}
+            Some(9) => {
+                result.push(next_point);
             }
-            _ => 0,
-        }
+            _ => {
+                result.extend(self.valid_reachable_peaks_from(next_point, next_point + UP));
+                result.extend(self.valid_reachable_peaks_from(next_point, next_point + RIGHT));
+                result.extend(self.valid_reachable_peaks_from(next_point, next_point + DOWN));
+                result.extend(self.valid_reachable_peaks_from(next_point, next_point + LEFT));
+            }
+        };
+
+        result
     }
 
     fn bases(&self) -> HashSet<Point2d<i32>> {
         self.points_with_height(0)
-    }
-
-    fn peaks(&self) -> HashSet<Point2d<i32>> {
-        self.points_with_height(9)
     }
 
     fn points_with_height(&self, height: u32) -> HashSet<Point2d<i32>> {
@@ -161,7 +164,7 @@ mod tests {
     }
 
     #[test]
-    fn test_map_peaks() {
+    fn test_map_number_of_reachable_peaks() {
         let input = [
             String::from("10..9.."),
             String::from("2...8.."),
@@ -174,15 +177,27 @@ mod tests {
 
         let map = Map::new(&input);
 
-        let expected = HashSet::from([Point2d::new(4, 0), Point2d::new(3, 5)]);
-
-        let result = map.peaks();
-
-        assert_eq!(result, expected);
+        assert_eq!(map.number_of_reachable_peaks(), 3);
     }
 
     #[test]
-    fn test_map_number_of_peak_paths_from() {
+    fn test_map_number_of_distinct_valid_paths() {
+        let input = [
+            String::from("012345"),
+            String::from("123456"),
+            String::from("234567"),
+            String::from("345678"),
+            String::from("4.6789"),
+            String::from("56789."),
+        ];
+
+        let map = Map::new(&input);
+
+        assert_eq!(map.number_of_distinct_valid_paths(), 227);
+    }
+
+    #[test]
+    fn test_map_number_of_reachable_peaks_from() {
         let input = [
             String::from("..90..9"),
             String::from("...1.98"),
@@ -194,7 +209,80 @@ mod tests {
         ];
 
         let map = Map::new(&input);
+        let base_point = Point2d::new(3, 0);
 
-        assert_eq!(map.number_of_peak_paths_from(Point2d::new(3, 0)), 4);
+        assert_eq!(map.number_of_reachable_peaks_from(base_point), 4);
+    }
+    #[test]
+    fn test_map_number_of_distinct_valid_paths_from() {
+        let input = [
+            String::from(".....0."),
+            String::from("..4321."),
+            String::from("..5..2."),
+            String::from("..6543."),
+            String::from("..7..4."),
+            String::from("..8765."),
+            String::from("..9...."),
+        ];
+
+        let map = Map::new(&input);
+        let base_point = Point2d::new(5, 0);
+
+        assert_eq!(map.number_of_distinct_valid_paths_from(base_point), 3);
+    }
+
+    #[test]
+    fn test_map_valid_reachable_peaks_from_empty() {
+        let input = [
+            String::from("..90..9"),
+            String::from("...1.98"),
+            String::from("...2..7"),
+            String::from("6543456"),
+            String::from("765.987"),
+            String::from("876...."),
+            String::from("987...."),
+        ];
+
+        let map = Map::new(&input);
+        let base_point = Point2d::new(3, 0);
+
+        let result = map.valid_reachable_peaks_from(base_point, base_point + LEFT);
+
+        assert!(result.is_empty());
+    }
+    #[test]
+    fn test_map_valid_reachable_peaks_from_non_empty() {
+        let input = [
+            String::from("..90..9"),
+            String::from("...1.98"),
+            String::from("...2..7"),
+            String::from("6543456"),
+            String::from("765.987"),
+            String::from("876...."),
+            String::from("987...."),
+        ];
+
+        let map = Map::new(&input);
+        let base_point = Point2d::new(3, 0);
+
+        let expected = vec![
+            Point2d::new(6, 0),
+            Point2d::new(5, 1),
+            Point2d::new(4, 4),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+            Point2d::new(0, 6),
+        ];
+
+        let result = map.valid_reachable_peaks_from(base_point, base_point + DOWN);
+
+        assert_eq!(result, expected);
     }
 }
