601ce29de5291de40a736194fb96b293e9977403
Dylan Hicks <dman1366@gmail.com>
2024-12-24 14:51:14 -0700

    init day 16

---
+++ src/day_16/mod.rs
@@ -0,0 +1,37 @@
+use crate::util::file_reader::to_string_vector;
+
+pub fn run() {
+    let input = to_string_vector("inputs/day_16.txt").expect("Something went wrong with Day 16!");
+
+    println!("Day 16 Part 1: {:?}", part_1(&input));
+    println!("Day 16 Part 2: {:?}", part_2(&input));
+}
+
+fn part_1(_input: &[String]) -> usize {
+    unimplemented!()
+}
+
+fn part_2(_input: &[String]) -> usize {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "File not found!")]
+    fn test_part_1() {
+        let input = to_string_vector("test_inputs/day_16.txt").unwrap();
+
+        assert_eq!(part_1(&input), 666);
+    }
+
+    #[test]
+    #[should_panic(expected = "File not found!")]
+    fn test_part_2() {
+        let input = to_string_vector("test_inputs/day_16.txt").unwrap();
+
+        assert_eq!(part_2(&input), 666);
+    }
+}
