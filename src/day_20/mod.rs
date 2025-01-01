9740bf509ec7b6fbf719634e39eaa09462629724
Dylan Hicks <dman1366@gmail.com>
2025-01-01 13:41:34 -0700

    Finish day 20.

--- src/day_20/mod.rs
+++ src/day_20/mod.rs
@@ -8,17 +8,28 @@ pub fn run() {
     let input = to_string_vector("inputs/day_20.txt").expect("Something went wrong with Day 20!");
 
     println!("Day 20 Part 1: {:?}", part_1(&input, 100));
-    println!("Day 20 Part 2: {:?}", part_2(&input));
+    println!("Day 20 Part 2: {:?}", part_2(&input, 100));
 }
 
-fn part_1(input: &[String], time_to_save: usize) -> usize {
-    let race = Race::from(input);
-
-    race.number_of_cheats_to_save(time_to_save)
+fn part_1(input: &[String], time_to_save_min: usize) -> usize {
+    number_of_cheats_to_save_at_least(input, 2, time_to_save_min)
 }
 
-fn part_2(_input: &[String]) -> usize {
-    unimplemented!()
+fn part_2(input: &[String], time_to_save_min: usize) -> usize {
+    number_of_cheats_to_save_at_least(input, 20, time_to_save_min)
+}
+
+fn number_of_cheats_to_save_at_least(
+    input: &[String],
+    cheat_duration: usize,
+    time_to_save_min: usize,
+) -> usize {
+    Race::from(input)
+        .cheats_to_save(cheat_duration)
+        .iter()
+        .filter(|(time_saved, _)| time_to_save_min <= **time_saved)
+        .map(|(_, cheats)| cheats.len())
+        .sum()
 }
 
 #[cfg(test)]
@@ -29,14 +40,13 @@ mod tests {
     fn test_part_1() {
         let input = to_string_vector("test_inputs/day_20.txt").unwrap();
 
-        assert_eq!(part_1(&input, 12), 3);
+        assert_eq!(part_1(&input, 1), 44);
     }
 
     #[test]
-    #[should_panic(expected = "not implemented")]
     fn test_part_2() {
         let input = to_string_vector("test_inputs/day_20.txt").unwrap();
 
-        assert_eq!(part_2(&input), 666);
+        assert_eq!(part_2(&input, 50), 285);
     }
 }
--- src/day_20/race.rs
+++ src/day_20/race.rs
@@ -1,5 +1,4 @@
-use std::cmp::{Ordering, Reverse};
-use std::collections::{BinaryHeap, HashMap, HashSet};
+use std::collections::{HashMap, HashSet};
 use std::fmt;
 
 use crate::util::grid::{Direction, Grid};
@@ -39,70 +38,8 @@ impl fmt::Display for Item {
     }
 }
 
-#[derive(Debug, Eq, Clone, Copy)]
-struct SearchNode {
-    payload: usize,
-    from_point: Point2d<i32>,
-    point: Point2d<i32>,
-    can_cheat: bool,
-}
-
-impl SearchNode {
-    fn new(payload: usize, point: Point2d<i32>) -> Self {
-        SearchNode {
-            payload,
-            from_point: point,
-            point,
-            can_cheat: true,
-        }
-    }
-
-    #[must_use]
-    fn move_to(self, direction: Direction) -> Self {
-        SearchNode {
-            payload: self.payload + 1,
-            from_point: self.point,
-            point: self.point + direction.as_offset(),
-            can_cheat: self.can_cheat,
-        }
-    }
-
-    #[must_use]
-    fn move_to_without_cheat(self, direction: Direction) -> Self {
-        SearchNode {
-            payload: self.payload + 1,
-            from_point: self.point,
-            point: self.point + direction.as_offset(),
-            can_cheat: false,
-        }
-    }
-
-    #[must_use]
-    fn rev(self) -> Reverse<Self> {
-        Reverse(self)
-    }
-}
-
-impl PartialOrd for SearchNode {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl Ord for SearchNode {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.payload.cmp(&other.payload)
-    }
-}
-
-impl PartialEq for SearchNode {
-    fn eq(&self, other: &Self) -> bool {
-        self.payload == other.payload
-    }
-}
-
-#[derive(Debug, PartialEq, Clone, Copy)]
-struct Cheat {
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+pub struct Cheat {
     from_point: Point2d<i32>,
     to_point: Point2d<i32>,
 }
@@ -116,12 +53,6 @@ impl Cheat {
     }
 }
 
-impl From<SearchNode> for Cheat {
-    fn from(node: SearchNode) -> Self {
-        Cheat::new(node.from_point, node.point)
-    }
-}
-
 #[derive(Debug, PartialEq)]
 pub struct Race {
     map: Grid<Item>,
@@ -130,73 +61,56 @@ pub struct Race {
 }
 
 impl Race {
-    pub fn number_of_cheats_to_save(&self, time_to_save: usize) -> usize {
+    pub fn cheats_to_save(&self, cheat_duration: usize) -> HashMap<usize, HashSet<Cheat>> {
         let path = self.path_to_end();
-        let time_to_end = path.len().saturating_sub(1);
-        let target_time = time_to_end.saturating_sub(time_to_save);
 
-        let mut result = HashSet::new();
-        let mut seen = HashSet::new();
-        let mut heap = BinaryHeap::from([Reverse(SearchNode::new(0, self.start))]);
+        let mut result = HashMap::new();
 
-        while let Some(Reverse(node)) = heap.pop() {
-            let saves_target_time = match path.get(&node.point) {
-                Some(original_time) => *original_time == node.payload + time_to_save,
-                None => false,
-            };
+        for (current_time, point) in path.iter().enumerate() {
+            for (time_to_next, next_point) in path.iter().enumerate().skip(current_time + 1) {
+                let distance_between = Self::distance_between(*point, *next_point);
 
-            let inserted = seen.insert((node.point, node.can_cheat));
+                let time_saved = time_to_next.saturating_sub(distance_between + current_time);
+                let can_reach_by_cheating = distance_between <= cheat_duration;
 
-            match self.map.get(node.point) {
-                _ if path.contains_key(&node.point) && !node.can_cheat => {
-                    seen.remove(&(node.point, node.can_cheat));
-
-                    if saves_target_time {
-                        result.insert(node.from_point);
-                    }
+                if can_reach_by_cheating && (time_saved != 0) {
+                    result
+                        .entry(time_saved)
+                        .or_insert(HashSet::new())
+                        .insert(Cheat::new(*point, *next_point));
                 }
-                Some(Item::Wall) if inserted && node.can_cheat && node.payload < target_time => {
-                    heap.push(node.move_to_without_cheat(Direction::Up).rev());
-                    heap.push(node.move_to_without_cheat(Direction::Right).rev());
-                    heap.push(node.move_to_without_cheat(Direction::Down).rev());
-                    heap.push(node.move_to_without_cheat(Direction::Left).rev());
-                }
-                Some(Item::Empty | Item::Start) if inserted && node.payload < target_time => {
-                    heap.push(node.move_to(Direction::Up).rev());
-                    heap.push(node.move_to(Direction::Right).rev());
-                    heap.push(node.move_to(Direction::Down).rev());
-                    heap.push(node.move_to(Direction::Left).rev());
-                }
-                _ => {}
             }
         }
 
-        result.len()
+        result
     }
 
-    fn time_to_end(&self) -> usize {
-        self.path_to_end().len().saturating_sub(1)
-    }
+    fn path_to_end(&self) -> Vec<Point2d<i32>> {
+        let mut path = Vec::new();
+        let mut seen = HashSet::new();
+        let mut processing = vec![self.start];
 
-    fn path_to_end(&self) -> HashMap<Point2d<i32>, usize> {
-        let mut path = HashMap::new();
-        let mut heap = BinaryHeap::from([Reverse(SearchNode::new(0, self.start))]);
+        while let Some(point) = processing.pop() {
+            match self.map.get(point) {
+                Some(Item::Empty | Item::Start | Item::End) if seen.insert(point) => {
+                    path.push(point);
 
-        while let Some(Reverse(node)) = heap.pop() {
-            match self.map.get(node.point) {
-                Some(Item::Empty | Item::Start | Item::End) if !path.contains_key(&node.point) => {
-                    path.insert(node.point, node.payload);
-
-                    heap.push(node.move_to(Direction::Up).rev());
-                    heap.push(node.move_to(Direction::Right).rev());
-                    heap.push(node.move_to(Direction::Down).rev());
-                    heap.push(node.move_to(Direction::Left).rev());
+                    processing.push(point + Direction::Up.as_offset());
+                    processing.push(point + Direction::Right.as_offset());
+                    processing.push(point + Direction::Down.as_offset());
+                    processing.push(point + Direction::Left.as_offset());
                 }
                 _ => {}
             }
         }
 
         path
+    }
+
+    fn distance_between(point_a: Point2d<i32>, point_b: Point2d<i32>) -> usize {
+        let difference = point_b - point_a;
+
+        usize::try_from(difference.x.abs() + difference.y.abs()).unwrap()
     }
 }
 
@@ -256,16 +170,16 @@ mod tests {
     fn test_race_path_to_end() {
         let race = Race::from(["#####", "#...#", "#.#.#", "#S#.E", "#####"]);
 
-        let expected = HashMap::from([
-            (Point2d::new(1, 3), 0),
-            (Point2d::new(1, 2), 1),
-            (Point2d::new(1, 1), 2),
-            (Point2d::new(2, 1), 3),
-            (Point2d::new(3, 1), 4),
-            (Point2d::new(3, 2), 5),
-            (Point2d::new(3, 3), 6),
-            (Point2d::new(4, 3), 7),
-        ]);
+        let expected = vec![
+            Point2d::new(1, 3),
+            Point2d::new(1, 2),
+            Point2d::new(1, 1),
+            Point2d::new(2, 1),
+            Point2d::new(3, 1),
+            Point2d::new(3, 2),
+            Point2d::new(3, 3),
+            Point2d::new(4, 3),
+        ];
 
         let result = race.path_to_end();
 
@@ -292,11 +206,11 @@ mod tests {
             "###############",
         ]);
 
-        assert_eq!(race.time_to_end(), 84);
+        assert_eq!(race.path_to_end().len().saturating_sub(1), 84);
     }
 
     #[test]
-    fn test_race_number_of_cheats_to_save() {
+    fn test_race_cheats_to_save_cheat_duration_2() {
         let race = Race::from([
             "###############",
             "#...#...#.....#",
@@ -315,16 +229,58 @@ mod tests {
             "###############",
         ]);
 
-        assert_eq!(race.number_of_cheats_to_save(2), 14);
-        assert_eq!(race.number_of_cheats_to_save(4), 14);
-        assert_eq!(race.number_of_cheats_to_save(6), 2);
-        assert_eq!(race.number_of_cheats_to_save(8), 4);
-        assert_eq!(race.number_of_cheats_to_save(10), 2);
-        assert_eq!(race.number_of_cheats_to_save(12), 3);
-        assert_eq!(race.number_of_cheats_to_save(20), 1);
-        assert_eq!(race.number_of_cheats_to_save(36), 1);
-        assert_eq!(race.number_of_cheats_to_save(38), 1);
-        assert_eq!(race.number_of_cheats_to_save(40), 1);
-        assert_eq!(race.number_of_cheats_to_save(64), 1);
+        let result = race.cheats_to_save(2);
+
+        assert_eq!(result.len(), 11);
+        assert_eq!(result.values().map(HashSet::len).sum::<usize>(), 44);
+        assert_eq!(result.get(&2).unwrap().len(), 14);
+        assert_eq!(result.get(&4).unwrap().len(), 14);
+        assert_eq!(result.get(&6).unwrap().len(), 2);
+        assert_eq!(result.get(&8).unwrap().len(), 4);
+        assert_eq!(result.get(&10).unwrap().len(), 2);
+        assert_eq!(result.get(&12).unwrap().len(), 3);
+        assert_eq!(result.get(&20).unwrap().len(), 1);
+        assert_eq!(result.get(&36).unwrap().len(), 1);
+        assert_eq!(result.get(&38).unwrap().len(), 1);
+        assert_eq!(result.get(&40).unwrap().len(), 1);
+        assert_eq!(result.get(&64).unwrap().len(), 1);
+    }
+
+    #[test]
+    fn test_race_cheats_to_save_cheat_duration_20() {
+        let race = Race::from([
+            "###############",
+            "#...#...#.....#",
+            "#.#.#.#.#.###.#",
+            "#S#...#.#.#...#",
+            "#######.#.#.###",
+            "#######.#.#...#",
+            "#######.#.###.#",
+            "###..E#...#...#",
+            "###.#######.###",
+            "#...###...#...#",
+            "#.#####.#.###.#",
+            "#.#...#.#.#...#",
+            "#.#.#.#.#.#.###",
+            "#...#...#...###",
+            "###############",
+        ]);
+
+        let result = race.cheats_to_save(20);
+
+        assert_eq!(result.get(&50).unwrap().len(), 32);
+        assert_eq!(result.get(&52).unwrap().len(), 31);
+        assert_eq!(result.get(&54).unwrap().len(), 29);
+        assert_eq!(result.get(&56).unwrap().len(), 39);
+        assert_eq!(result.get(&58).unwrap().len(), 25);
+        assert_eq!(result.get(&60).unwrap().len(), 23);
+        assert_eq!(result.get(&62).unwrap().len(), 20);
+        assert_eq!(result.get(&64).unwrap().len(), 19);
+        assert_eq!(result.get(&66).unwrap().len(), 12);
+        assert_eq!(result.get(&68).unwrap().len(), 14);
+        assert_eq!(result.get(&70).unwrap().len(), 12);
+        assert_eq!(result.get(&72).unwrap().len(), 22);
+        assert_eq!(result.get(&74).unwrap().len(), 4);
+        assert_eq!(result.get(&76).unwrap().len(), 3);
     }
 }
